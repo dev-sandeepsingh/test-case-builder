4b2303daa0e5cc1dc87e1fd6edd0d5ccc6d4e9be
sandeep singh <sandeepsingh@ucreate.co.in>
2019-11-18 13:34:40 +0530

    Create test cases builder  application

---
+++ src/app/testcase/testcase.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testcase',
+  templateUrl: './testcase.component.html',
+  styleUrls: ['./testcase.component.scss']
+})
+export class TestcaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

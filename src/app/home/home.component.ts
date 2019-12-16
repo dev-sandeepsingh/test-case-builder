a6d93eaa7326b5f59174202f1391c881b0d4d5ed
SandeepSinghUcreate <sandeepsingh@ucreate.co.in>
2019-12-16 16:58:14 +0530

    Add blur event on text area

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,7 +27,7 @@ export class HomeComponent implements OnInit {
     })
   }
 
-  onBlurMethod(e) {
+  onBlurMethod() {
     this.keys = this.getKeys({ jsonData: this.testCaseBuilder.jsonDataText });
   }
 
